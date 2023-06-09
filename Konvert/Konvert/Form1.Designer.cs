b1b5c4075c85fecf148db15366943687e765a066
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-06-09 18:20:28 +0300

    Add files via upload

---
+++ Konvert/Konvert/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Konvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.RomanTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(67, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 41);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Конвертировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // RomanTextBox
+            // 
+            this.RomanTextBox.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RomanTextBox.Location = new System.Drawing.Point(46, 101);
+            this.RomanTextBox.Multiline = true;
+            this.RomanTextBox.Name = "RomanTextBox";
+            this.RomanTextBox.Size = new System.Drawing.Size(187, 28);
+            this.RomanTextBox.TabIndex = 1;
+            this.RomanTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(296, 101);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(98, 28);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(42, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Введите Римские цифры";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(318, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ответ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(439, 368);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.RomanTextBox);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox RomanTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Konvert/Konvert/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Konvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private int ConvertRomanToLatin(string romanNumber)
+        {
+            // создаем словарь соответствий для каждой римской цифры
+            Dictionary<char, int> romanValues = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+
+            int result = 0; // инициализируем результат
+            int previousValue = 0; // хранит значение предыдущей цифры
+            foreach (char c in romanNumber)
+            {
+                if (!romanValues.ContainsKey(c)) // проверяем, что введена корректная цифра
+                {
+                    return -1;
+                }
+
+                int currentValue = romanValues[c]; // текущее значение цифры
+                if (currentValue > previousValue) // вычитаем предыдущую цифру, если она меньше текущей
+                {
+                    result -= 2 * previousValue; // умножаем на 2, так как предыдущую цифру мы уже прибавили
+                }
+                result += currentValue; // добавляем текущую цифру к результату
+                previousValue = currentValue; // запоминаем текущую цифру
+            }
+
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string romanNumber = RomanTextBox.Text.ToUpper(); // переводим римскую цифру в верхний регистр
+            int latinNumber = ConvertRomanToLatin(romanNumber); // конвертируем
+            if (latinNumber != -1) // проверяем, что конвертация прошла успешно
+            {
+                textBox2.Text = latinNumber.ToString(); // выводим результат
+            }
+            else
+            {
+                MessageBox.Show("Некорректная римская цифра!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
